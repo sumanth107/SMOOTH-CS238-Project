a9245af6934e07c4fe1705f0cb3c09a0a572b2d3
Anirudh Sriram <anirudhsriram30799@gmail.com>
2023-11-27 16:55:59 -0800

    Added Sarsa(lamda), Q(lambda) & DQN

---
+++ DQN.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import collections
+from SimpleCrossWalkEnv import SmoothCrosswalkEnv
+from config import CONFIG
+from utils import plot_test_performance
+
+
+class DQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_dim)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+
+
+class DQNAgent:
+    def __init__(self, input_dim, output_dim, lr):
+        self.dqn = DQN(input_dim, output_dim)
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(self.dqn.parameters(), lr=lr)
+
+    def choose_action(self, state):
+        state = torch.tensor(state, dtype=torch.float)
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+        q_values = self.dqn(state)
+        action = torch.argmax(q_values, dim=1)
+        return action.item()
+
+    def learn(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        reward = torch.tensor(reward, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            reward = torch.unsqueeze(reward, 0)
+            action = torch.unsqueeze(action, 0)
+            done = (done, )
+
+        # Compute Q(s, a) - the model computes Q(s), then we select the columns of actions taken
+        q_values = self.dqn(state)
+        q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+
+        # Compute Q(s_{t+1}) for all next states.
+        next_q_values = self.dqn(next_state)
+        next_q_value = next_q_values.max(1)[0]
+
+        # Compute the expected Q values
+        expected_q_value = reward + 0.99 * next_q_value * (1 - torch.tensor(done, dtype=torch.float))
+
+        # Compute Huber loss
+        loss = self.criterion(q_value, expected_q_value.detach())
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+
+if __name__ == "__main__":
+    random.seed(1)
+    np.random.seed(1)
+
+    n_steps = 10000
+    RANDOM_RUNS = 5
+    results = collections.defaultdict(list)
+
+    for _ in range(RANDOM_RUNS):
+        env = SmoothCrosswalkEnv(CONFIG)
+        agent = DQNAgent(env.observation_space.shape[0], env.action_space.n, lr=0.001)
+
+        total_rewards = []
+        for episode in range(n_steps):
+            state = env.reset()
+            total_reward = 0
+            done = False
+
+            while not done:
+                action = agent.choose_action(state)
+                next_state, reward, done, info = env.step(action)
+                agent.learn(state, action, reward, next_state, done)
+                state = next_state
+                total_reward += reward
+
+            total_rewards.append(total_reward)
+            print(f"Episode {episode + 1}: Total Reward = {total_reward}")
+
+        # Assuming you have a function called plot_test_performance
+        plot_test_performance(total_rewards, 'dqn_results.png')
+
+        avg_reward = sum(total_rewards) / n_steps
+        print("Average Total Reward: {}".format(avg_reward))
---
+++ sarsa_lambda.py
@@ -0,0 +1,131 @@
+import numpy as np
+import random
+import collections
+from SimpleCrossWalkEnv import SmoothCrosswalkEnv
+from config import CONFIG
+from utils import plot_test_performance
+
+
+class SARSA_Lambda:
+    def __init__(self, env, alpha=0.5, gamma=0.95, epsilon=0.0, epsilon_decay=0.99, min_epsilon=0.01, lamb=0.9):
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
+        self.lamb = lamb
+        # Initialize Q-table and eligibility traces
+        position_size = int(env.observation_space.high[0]) + 1
+        velocity_size = int(env.observation_space.high[1]) + 1
+        self.Q = np.zeros((position_size, velocity_size, env.action_space.n))
+        self.N = np.zeros((position_size, velocity_size, env.action_space.n))
+
+    def choose_action(self, state):
+        # Using Epsilon-greedy policy
+        if np.random.uniform(0, 1) < self.epsilon:
+            # Explore
+            return self.env.action_space.sample()
+        else:
+            # Exploit
+            position, velocity = int(state[0]), int(state[1])
+            return np.argmax(self.Q[position, velocity])
+
+    def learn(self, state, action, reward, next_state, next_action, done):
+        position, velocity = int(state[0]), int(state[1])
+        next_position, next_velocity = int(next_state[0]), int(next_state[1])
+
+        # Ensure action is an integer
+        action = int(action)
+
+        # Update Q-value and eligibility traces
+        delta = reward + self.gamma * self.Q[next_position, next_velocity, next_action] - self.Q[position, velocity, action]
+        self.N[position, velocity, action] += 1
+
+        for pos in range(self.Q.shape[0]):
+            for vel in range(self.Q.shape[1]):
+                for act in range(self.Q.shape[2]):
+                    self.Q[pos, vel, act] += self.alpha * delta * self.N[pos, vel, act]
+                    self.N[pos, vel, act] = self.gamma * self.lamb * self.N[pos, vel, act]
+
+        # Update epsilon
+        if not done:
+            self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
+
+    def train(self, num_episodes):
+        rewards = []
+        for episode in range(num_episodes):
+            state = self.env.reset()
+            state = (int(state[0]), int(state[1]))  # Convert to discrete state
+            total_reward = 0
+            done = False
+
+            # Reset eligibility traces
+            self.N = np.zeros_like(self.N)
+
+            while not done:
+                action = self.choose_action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                next_action = self.choose_action(next_state)
+                next_state = (int(next_state[0]), int(next_state[1]))
+                self.learn(state, action, reward, next_state, next_action, done)
+                state = next_state
+                total_reward += reward
+
+            rewards.append(total_reward)
+            print(f"Episode {episode + 1}: Total Reward = {total_reward}")
+        plot_test_performance(rewards, 'sarsa_lambda_results.png')
+
+
+
+if __name__ == "__main__":
+    random.seed(1)
+    np.random.seed(1)
+
+    n_steps = 1000
+    RANDOM_RUNS = 5
+    results = collections.defaultdict(list)
+
+    for _ in range(RANDOM_RUNS):
+        # Train
+        env = SmoothCrosswalkEnv(CONFIG)
+        model = SARSA_Lambda(env)
+        model.train(1000)
+
+        # Test
+        state = env.reset()
+        total_reward = 0
+        step_to_goal = 0
+        total_jerk = 0
+        max_jerk = -1
+        speeding = False
+        success = False
+        for step in range(n_steps):
+            discrete_state = (int(state[0]), int(state[1]))
+            action = model.choose_action(discrete_state)
+            state, reward, done, info = env.step(action)
+            total_reward += reward
+            total_jerk += info['jerk']
+            if info['speeding']:
+                speeding = True
+            max_jerk = max(max_jerk, info['jerk'])
+            if done:
+                step_to_goal = step
+                if reward == 39:
+                    success = True
+                break
+        results['total_reward'].append(total_reward)
+        results['steps'].append(step_to_goal)
+        results['total_jerk'].append(total_jerk)
+        results['max_jerk'].append(max_jerk)
+        results['speeding'].append(1 if speeding else 0)
+        results['success'].append(1 if success else 0)
+
+    avg_reward = sum(results['total_reward']) / RANDOM_RUNS
+    print("Average Total Reward: {}".format(avg_reward))
+    avg_steps = sum(results['steps']) / RANDOM_RUNS
+    print("Average Steps: {}".format(avg_steps))
+    sucess_rate = sum(results['success']) * 100 / RANDOM_RUNS
+    print("Success Rate: {}%".format(sucess_rate))
+    speeding_rate = sum(results['speeding']) * 100 / RANDOM_RUNS
+    print("Speeding Rate: {}%".format(speeding_rate))
